99831be7d2374baa815b5cab1323be534aae9403
jacob1 <jfu614@gmail.com>
2016-03-23 20:32:16 -0400

    33.4 build 70: Update internal version number to 91.1.325

--- includes/defines.h
+++ includes/defines.h
@@ -25,18 +25,18 @@
  
 //VersionInfoStart
 #define SAVE_VERSION 91
-#define MINOR_VERSION 0
-#define BUILD_NUM 324
+#define MINOR_VERSION 1
+#define BUILD_NUM 325
 //VersionInfoEnd
 
 #define MOD_VERSION 33
 #define MOD_MINOR_VERSION 4
 #define MOD_SAVE_VERSION 22 //This is not the version number of my mod anymore, it's only changed when I change the saving code
-#define MOD_BUILD_VERSION 69 //For update checks
+#define MOD_BUILD_VERSION 70 //For update checks
 #define BETA_VERSION 91
 #define BETA_MINOR_VER 0
 #define RELEASE_VERSION 91
-#define RELEASE_MINOR_VER 0
+#define RELEASE_MINOR_VER 1
 
 #define MOBILE_MAJOR 0
 #define MOBILE_MINOR 3
