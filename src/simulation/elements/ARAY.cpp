dddbc92f6fdc4b7311eb1df3c10c8e6fc863cf44
jacob1 <jfu614@gmail.com>
2016-05-28 11:18:29 -0400

    some ARAY fix from a while ago (fixes BRAY releasing from STOR)

--- src/simulation/elements/ARAY.cpp
+++ src/simulation/elements/ARAY.cpp
@@ -107,7 +107,7 @@ int ARAY_update(UPDATE_FUNC_ARGS)
 										{
 											for (int rx1 = 0; rx1 >= -1 && rx1 <= 1; rx1 = -rx1-rx1+1)
 											{
-												int np = sim->part_create(-1, x+nxi+nxx+rx1, y+nyi+nyy+ry1, parts[r>>8].tmp&0xFF);
+												int np = sim->part_create(-1, x+nxi+nxx+rx1, y+nyi+nyy+ry1, parts[r].tmp&0xFF);
 												if (np != -1)
 												{
 													parts[np].temp = parts[r].temp;
@@ -123,7 +123,7 @@ int ARAY_update(UPDATE_FUNC_ARGS)
 									}
 									else
 									{
-										parts[r>>8].life = 10;
+										parts[r].life = 10;
 									}
 								}
 								//this if prevents BRAY from stopping on certain materials
