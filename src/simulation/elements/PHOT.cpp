0891be64efd1b2c14bdb224fcf4415ce5b93a874
jacob1 <jfu614@gmail.com>
2017-05-16 23:39:55 -0400

    Fix QRTZ killing life=0 PHOT b0f3b2a36bb6

--- src/simulation/elements/PHOT.cpp
+++ src/simulation/elements/PHOT.cpp
@@ -62,7 +62,8 @@ int PHOT_update(UPDATE_FUNC_ARGS)
 						parts[i].vy = 3.0f*sinf(a);
 						if (parts[i].ctype == 0x3FFFFFFF)
 							parts[i].ctype = 0x1F<<(rand()%26);
-						parts[i].life++; //Delay death
+						if (parts[i].life)
+							parts[i].life++; //Delay death
 					}
 				}
 				else if ((r&0xFF) == PT_BGLA)
