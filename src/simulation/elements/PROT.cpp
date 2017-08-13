Wowowowowowowowowowowowowowoowo/The-Powder-Toy

commit aabf7e060cb65e3fd4f3cd693b1be079eaa72307

tpt++/cracker64: More element optimizations! a115e7895233 be6551ceea48

src/simulation/elements/PROT.cpp
@@ -40,68 +40,62 @@ int PROT_update(UPDATE_FUNC_ARGS)
 {
 	pv[y/CELL][x/CELL] -= .003f;
 	int under = pmap[y][x];
-	//set off explosives (only when hot because it wasn't as fun when it made an entire save explode)
-	if (parts[i].temp > 273.15f+500.0f && (sim->elements[under&0xFF].Flammable || sim->elements[under&0xFF].Explosive || (under&0xFF) == PT_BANG))
+	int utype = under & 0xFF;
+	switch (utype)
 	{
-		sim->part_create(under>>8, x, y, PT_FIRE);
-		parts[under>>8].temp += restrict_flt(sim->elements[under&0xFF].Flammable*5.0f, MIN_TEMP, MAX_TEMP);
-		pv[y/CELL][x/CELL] += 1.00f;
-	}
-	//remove active sparks
-	else if ((under&0xFF) == PT_SPRK)
-	{
+	case PT_SPRK:
+		//remove active sparks
 		part_change_type(under>>8, x, y, parts[under>>8].ctype);
 		parts[under>>8].life = 44+parts[under>>8].life;
 		parts[under>>8].ctype = 0;
-	}
-	else if ((under&0xFF) == PT_DEUT)
-	{
+		break;
+	case PT_DEUT:
 		if ((-((int)pv[y/CELL][x/CELL]-4)+(parts[under>>8].life/100)) > rand()%200)
 		{
 			DeutImplosion(sim, parts[under>>8].life, x, y, restrict_flt(parts[under>>8].temp + parts[under>>8].life*500, MIN_TEMP, MAX_TEMP), PT_PROT);
 			kill_part(under>>8);
 		}
-	}
-	//prevent inactive sparkable elements from being sparked
-	else if ((sim->elements[under&0xFF].Properties&PROP_CONDUCTS) && parts[under>>8].life <= 4)
-	{
-		parts[under>>8].life = 40+parts[under>>8].life;
-	}
-	//Powered LCRY reaction: PROT->PHOT
-	else if ((under&0xFF) == PT_LCRY && parts[under>>8].life > 5 && !(rand()%10))
-	{
+		break;
+	case PT_LCRY:
+		//Powered LCRY reaction: PROT->PHOT
 		part_change_type(i, x, y, PT_PHOT);
 		parts[i].life *= 2;
 		parts[i].ctype = 0x3FFFFFFF;
-	} 
-	else if ((under&0xFF) == PT_EXOT)
+		break;
+	case PT_EXOT:
 		parts[under>>8].ctype = PT_PROT;
-
-	//make temp of other things closer to it's own temperature. This will change temp of things that don't conduct, and won't change the PROT's temperature
-	if (under)
-	{
-		//now changed so that PROT goes through portal, so only the WIFI part applies
-		if ((under&0xFF) == PT_WIFI /*|| (under&0xFF) == PT_PRTI || (under&0xFF) == PT_PRTO*/)
-		{
-			float change;
-			if (parts[i].temp<173.15f) change = -1000.0f;
-			else if (parts[i].temp<273.15f) change = -100.0f;
-			else if (parts[i].temp>473.15f) change = 1000.0f;
-			else if (parts[i].temp>373.15f) change = 100.0f;
-			else change = 0.0f;
-			parts[under>>8].temp = restrict_flt(parts[under>>8].temp+change, MIN_TEMP, MAX_TEMP);
-		}
-		else
-		{
-			parts[under>>8].temp = restrict_flt(parts[under>>8].temp-(parts[under>>8].temp-parts[i].temp)/4.0f, MIN_TEMP, MAX_TEMP);
-		}
-	}
-	//else, slowly kill it if it's not inside an element
-	else if (parts[i].life)
-	{
+		break;
+	case PT_NONE:
 		if (!--parts[i].life)
 			kill_part(i);
+		break;
+	case PT_WIFI:
+		float change;
+		if (parts[i].temp<173.15f) change = -1000.0f;
+		else if (parts[i].temp<273.15f) change = -100.0f;
+		else if (parts[i].temp>473.15f) change = 1000.0f;
+		else if (parts[i].temp>373.15f) change = 100.0f;
+		else change = 0.0f;
+		parts[under>>8].temp = restrict_flt(parts[under>>8].temp+change, MIN_TEMP, MAX_TEMP);
+		break;
+	default:
+		//set off explosives (only when hot because it wasn't as fun when it made an entire save explode)
+		if (parts[i].temp > 273.15f+500.0f && (sim->elements[under&0xFF].Flammable || sim->elements[under&0xFF].Explosive || (under&0xFF) == PT_BANG))
+		{
+			sim->part_create(under>>8, x, y, PT_FIRE);
+			parts[under>>8].temp += restrict_flt(sim->elements[under&0xFF].Flammable*5.0f, MIN_TEMP, MAX_TEMP);
+			pv[y/CELL][x/CELL] += 1.00f;
+		}
+		//prevent inactive sparkable elements from being sparked
+		else if ((sim->elements[under&0xFF].Properties&PROP_CONDUCTS) && parts[under>>8].life <= 4)
+		{
+			parts[under>>8].life = 40+parts[under>>8].life;
+		}
+		break;
 	}
+	//make temp of other things closer to it's own temperature. This will change temp of things that don't conduct, and won't change the PROT's temperature
+	if (utype && utype != PT_WIFI)
+		parts[under>>8].temp = restrict_flt(parts[under>>8].temp-(parts[under>>8].temp-parts[i].temp)/4.0f, MIN_TEMP, MAX_TEMP);
 	
 	//if this proton has collided with another last frame, change it into a heavier element
 	if (parts[i].tmp)

src/simulation/elements/ACEL.cpp
@@ -31,7 +31,7 @@ int ACEL_update(UPDATE_FUNC_ARGS)
 	parts[i].tmp = 0;
 	for (rx=-1; rx<2; rx++)
 		for (ry=-1; ry<2; ry++)
-			if (BOUNDS_CHECK && (rx || ry) && !(rx && ry))
+			if (BOUNDS_CHECK && (!rx != !ry))
 			{
 				r = pmap[y+ry][x+rx];
 				if(!r)

src/simulation/elements/DRAY.cpp
@@ -18,10 +18,10 @@
 int DRAY_update(UPDATE_FUNC_ARGS)
 {
 	int ctype = parts[i].ctype&0xFF, ctypeExtra = parts[i].ctype>>8, copyLength = parts[i].tmp, copySpaces = parts[i].tmp2;
-	if (copySpaces <= 0)
-		copySpaces = 0;
-	if (copyLength <= 0)
-		copyLength = 0;
+	if (copySpaces < 0)
+		copySpaces = parts[i].tmp2  = 0;
+	if (copyLength < 0)
+		copyLength = parts[i].tmp  = 0;
 	else
 		copySpaces++; //strange hack
 	if (!parts[i].life) // only fire when life is 0, but nothing sets the life right now

src/simulation/elements/GOLD.cpp
@@ -17,17 +17,22 @@
 
 int GOLD_update(UPDATE_FUNC_ARGS)
 {
-	int rx, ry, r, j;
+	int rx, ry, r, j, rndstore;
 	static int checkCoordsX[] = { -4, 4, 0, 0 };
 	static int checkCoordsY[] = { 0, 0, -4, 4 };
 	//Find nearby rusted iron (BMTL with tmp 1+)
-	for(j = 0; j < 8; j++){
-		rx = (rand()%9)-4;
-		ry = (rand()%9)-4;
-		if ((!rx != !ry) && BOUNDS_CHECK) {
+	for (j = 0; j < 8; j++)
+	{
+		rndstore = rand();
+		rx = (rndstore % 9)-4;
+		rndstore >>= 4;
+		ry = (rndstore % 9)-4;
+		if ((!rx != !ry) && BOUNDS_CHECK)
+		{
 			r = pmap[y+ry][x+rx];
-			if(!r) continue;
-			if((r&0xFF)==PT_BMTL && parts[r>>8].tmp)
+			if (!r)
+				continue;
+			if ((r&0xFF)==PT_BMTL && parts[r>>8].tmp)
 			{
 				parts[r>>8].tmp = 0;
 				sim->part_change_type(r>>8, x+rx, y+ry, PT_IRON);
@@ -37,13 +42,16 @@ int GOLD_update(UPDATE_FUNC_ARGS)
 	//Find sparks
 	if(!parts[i].life)
 	{
-		for(j = 0; j < 4; j++){
+		for (j = 0; j < 4; j++)
+		{
 			rx = checkCoordsX[j];
 			ry = checkCoordsY[j];
-			if ((!rx != !ry) && BOUNDS_CHECK) {
+			if ((!rx != !ry) && BOUNDS_CHECK)
+			{
 				r = pmap[y+ry][x+rx];
-				if(!r) continue;
-				if((r&0xFF)==PT_SPRK && parts[r>>8].life && parts[r>>8].life<4)
+				if (!r)
+					continue;
+				if ((r&0xFF) == PT_SPRK && parts[r>>8].life && parts[r>>8].life < 4)
 				{
 					sim->spark_conductive(i, x, y);
 					return 1;
@@ -63,9 +71,12 @@ int GOLD_update(UPDATE_FUNC_ARGS)
 
 int GOLD_graphics(GRAPHICS_FUNC_ARGS)
 {
-	*colr += rand()%10-5;
-	*colg += rand()%10-5;
-	*colb += rand()%10-5;
+	int rndstore = rand();
+	*colr += (rndstore % 10) - 5;
+	rndstore >>= 4;
+	*colg += (rndstore % 10)- 5;
+	rndstore >>= 4;
+	*colb += (rndstore % 10) - 5;
 	return 0;
 }
 

src/simulation/elements/PLNT.cpp
@@ -17,7 +17,7 @@
 
 int PLNT_update(UPDATE_FUNC_ARGS)
 {
-	int r, rx, ry, np;
+	int r, rx, ry, np, rndstore;
 	for (rx=-1; rx<2; rx++)
 		for (ry=-1; ry<2; ry++)
 			if (BOUNDS_CHECK && (rx || ry))
@@ -49,11 +49,14 @@ int PLNT_update(UPDATE_FUNC_ARGS)
 					}
 					break;
 				case PT_WOOD:
-					if (surround_space && abs(rx+ry)<=2 && parts[i].tmp==1 && !(rand()%4))
+					rndstore = rand();
+					if (surround_space && abs(rx+ry)<=2 && parts[i].tmp==1 && !(rndstore%4))
 					{
-						int nnx = rand()%3 -1;
-						int nny = rand()%3 -1;
-						if (x+rx+nnx>=0 && y+ry+nny>0 && x+rx+nnx<XRES && y+ry+nny<YRES && (nnx || nny))
+						rndstore >>= 3;
+						int nnx = (rndstore%3) -1;
+						rndstore >>= 2;
+						int nny = (rndstore%3) -1;
+						if (nnx || nny)
 						{
 							if (pmap[y+ry+nny][x+rx+nnx])
 								continue;

src/simulation/elements/VIBR.cpp
@@ -17,7 +17,7 @@
 
 int VIBR_update(UPDATE_FUNC_ARGS)
 {
-	int r, rx, ry, transfer, trade;
+	int r, rx, ry, transfer, trade, rndstore;
 	if (!parts[i].life) //if not exploding
 	{
 		//Heat absorption code
@@ -51,9 +51,10 @@ int VIBR_update(UPDATE_FUNC_ARGS)
 		//Release sparks before explode
 		if (parts[i].life < 300)
 		{
-			int randstore = rand();
-			rx = randstore%3-1;
-			ry = (randstore>>2)%3-1;
+			rndstore = rand();
+			rx = rndstore%3-1;
+			ry = (rndstore>>2)%3-1;
+			rndstore = rndstore >> 4;
 			r = pmap[y+ry][x+rx];
 			if ((r&0xFF) && (r&0xFF) != PT_BREL && (ptypes[r&0xFF].properties&PROP_CONDUCTS) && !parts[r>>8].life)
 			{
@@ -63,9 +64,8 @@ int VIBR_update(UPDATE_FUNC_ARGS)
 		//Release all heat
 		if (parts[i].life < 500)
 		{
-			int random = rand();
-			rx = random%7-3;
-			ry = (random>>3)%7-3;
+			rx = rndstore%7-3;
+			ry = (rndstore>>3)%7-3;
 			if(BOUNDS_CHECK)
 			{
 				r = pmap[y+ry][x+rx];
@@ -81,16 +81,16 @@ int VIBR_update(UPDATE_FUNC_ARGS)
 		{
 			if (!parts[i].tmp2)
 			{
-				int random = rand(), index;
+				rndstore = rand();
 				sim->part_create(i, x, y, PT_EXOT);
 				parts[i].tmp2 = rand()%1000;
-				index = sim->part_create(-3,x+((random>>4)&3)-1,y+((random>>6)&3)-1,PT_ELEC);
+				int index = sim->part_create(-3,x+((rndstore>>4)&3)-1,y+((rndstore>>6)&3)-1,PT_ELEC);
 				if (index != -1)
 					parts[index].temp = 7000;
-				index = sim->part_create(-3,x+((random>>8)&3)-1,y+((random>>10)&3)-1,PT_PHOT);
+				index = sim->part_create(-3,x+((rndstore>>8)&3)-1,y+((rndstore>>10)&3)-1,PT_PHOT);
 				if (index != -1)
 					parts[index].temp = 7000;
-				index = sim->part_create(-1,x+((random>>12)&3)-1,y+rand()%3-1,PT_BREL);
+				index = sim->part_create(-1,x+((rndstore>>12)&3)-1,y+rand()%3-1,PT_BREL);
 				if (index != -1)
 					parts[index].temp = 7000;
 				parts[i].temp=9000;
@@ -112,8 +112,6 @@ int VIBR_update(UPDATE_FUNC_ARGS)
 			if (BOUNDS_CHECK && (rx || ry))
 			{
 				r = pmap[y+ry][x+rx];
-				if (!r)
-					r = photons[y+ry][x+rx];
 				if (!r)
 					continue;
 				if (parts[i].life)
@@ -142,6 +140,7 @@ int VIBR_update(UPDATE_FUNC_ARGS)
 					if ((r&0xFF) == PT_EXOT && !(rand()%25))
 					{
 						sim->part_create(i, x, y, PT_EXOT);
+						return 1;
 					}
 				}
 				//VIBR+ANAR=BVBR
@@ -153,9 +152,12 @@ int VIBR_update(UPDATE_FUNC_ARGS)
 			}
 	for (trade = 0; trade < 9; trade++)
 	{
-		int random = rand();
-		rx = random%7-3;
-		ry = (random>>3)%7-3;
+		if (!(trade%2))
+			rndstore = rand();
+		rx = rndstore%7-3;
+		rndstore >>= 3;
+		ry = rndstore%7-3;
+		rndstore >>= 3;
 		if (BOUNDS_CHECK && (rx || ry))
 		{
 			r = pmap[y+ry][x+rx];
@@ -182,13 +184,15 @@ int VIBR_graphics(GRAPHICS_FUNC_ARGS)
 	{
 		*colr = (int)(fabs(sin(exp((750.0f-cpart->life)/170)))*200.0f);
 		if (cpart->tmp2)
-			*colg = (int)(fabs(sin(exp((750.0f-cpart->life)/170)))*200.0f);
-		else
-			*colg = 255;
-		if (cpart->tmp2)
+		{
+			*colg = *colr;
 			*colb = 255;
+		}
 		else
-			*colb = (int)(fabs(sin(exp((750.0f-cpart->life)/170)))*200.0f);
+		{
+			*colg = 255;
+			*colb = *colr;
+		}
 		*firea = 90;
 		*firer = *colr;
 		*fireg = *colg;

src/simulation/elements/VINE.cpp
@@ -17,11 +17,15 @@
 
 int VINE_update(UPDATE_FUNC_ARGS)
 {
-	int r, np, rx =(rand()%3)-1, ry=(rand()%3)-1;
+	int r, np, rx, ry, rndstore = rand();
+	rx = (rndstore % 3) - 1;
+	rndstore >>= 2;
+	ry = (rndstore % 3) - 1;
+	rndstore >>= 2;
 	if (BOUNDS_CHECK && (rx || ry))
 	{
 		r = pmap[y+ry][x+rx];
-		if (!(rand()%15))
+		if (!(rndstore % 15))
 			part_change_type(i,x,y,PT_PLNT);
 		else if (!r)
 		{

src/simulation/elements/VIRS.cpp
@@ -30,33 +30,26 @@ int VIRS_update(UPDATE_FUNC_ARGS)
 			parts[i].tmp2 = 0;
 			parts[i].pavg[0] = 0;
 			parts[i].pavg[1] = 0;
-			return 0;
+			return 1;
 		}
+
+		//cured virus isn't allowed in below code
+		return 0;
 	}
 	//decrease pavg[1] so it slowly dies
 	if (parts[i].pavg[1] > 0)
 	{
-		if (((rndstore>>1)&0xD) < 1)
+		if (!(rndstore & 0x7) && --parts[i].pavg[1] <= 0)
 		{
-			parts[i].pavg[1]--;
-			//if pavg[1] is now 0 and it's not in the process of being cured, kill it
-			if (!parts[i].pavg[1] && !parts[i].pavg[0])
-			{
-				kill_part(i);
-				return 1;
-			}
+			kill_part(i);
+			return 1;
 		}
+		rndstore >>= 3;
 	}
 
-	//none of the things in the below loop happen while virus is being cured
-	if (parts[i].pavg[0])
-		return 0;
-
 	for (rx=-1; rx<2; rx++)
-	{
-		//reset rndstore, one random can last through 3 locations and reduce rand() calling by up to 6x as much
-		rndstore = rand();
 		for (ry=-1; ry<2; ry++)
+		{
 			if (BOUNDS_CHECK && (rx || ry))
 			{
 				r = pmap[y+ry][x+rx];
@@ -64,16 +57,16 @@ int VIRS_update(UPDATE_FUNC_ARGS)
 					continue;
 
 				//spread "being cured" state
-				if (((r&0xFF) == PT_VIRS || (r&0xFF) == PT_VRSS || (r&0xFF) == PT_VRSG) && parts[r>>8].pavg[0])
+				if (parts[r>>8].pavg[0] && ((r&0xFF) == PT_VIRS || (r&0xFF) == PT_VRSS || (r&0xFF) == PT_VRSG))
 				{
-					parts[i].pavg[0] = parts[r>>8].pavg[0] + (((rndstore&0x7)>>1) ? 2:1);
+					parts[i].pavg[0] = parts[r>>8].pavg[0] + ((rndstore & 0x3) ? 2:1);
 					return 0;
 				}
 				//soap cures virus
 				else if ((r&0xFF) == PT_SOAP)
 				{
 					parts[i].pavg[0] += 10;
-					if (!((rndstore&0x7)>>1))
+					if (!(rndstore & 0x3))
 						kill_part(r>>8);
 					return 0;
 				}
@@ -87,12 +80,12 @@ int VIRS_update(UPDATE_FUNC_ARGS)
 				}
 				else if ((r&0xFF) != PT_VIRS && (r&0xFF) != PT_VRSS && (r&0xFF) != PT_VRSG && !(ptypes[r&0xFF].properties&PROP_INDESTRUCTIBLE))
 				{
-					if (!((rndstore&0xF)>>1))
+					if (!(rndstore & 0x7))
 					{
 						parts[r>>8].tmp2 = (r&0xFF);
 						parts[r>>8].pavg[0] = 0;
 						if (parts[i].pavg[1])
-							parts[r>>8].pavg[1] = parts[i].pavg[1] + ((rndstore>>4) ? 1:0);
+							parts[r>>8].pavg[1] = parts[i].pavg[1] + 1;
 						else
 							parts[r>>8].pavg[1] = 0;
 						if (parts[r>>8].temp < 305.0f)
@@ -102,7 +95,7 @@ int VIRS_update(UPDATE_FUNC_ARGS)
 						else
 							part_change_type(r>>8,x+rx,y+ry,PT_VIRS);
 					}
-					rndstore = rndstore >> 5;
+					rndstore >>= 3;
 				}
 				//protons make VIRS last forever
 				else if ((photons[y+ry][x+rx]&0xFF) == PT_PROT)
@@ -110,7 +103,10 @@ int VIRS_update(UPDATE_FUNC_ARGS)
 					parts[i].pavg[1] = 0;
 				}
 			}
-	}
+			//reset rndstore only once, halfway through
+			else if (!rx && !ry)
+				rndstore = rand();
+		}
 	return 0;
 }
 

commit bd82dcbbca51defcfbc967150db9e34918d069ac

fix PROT being destroyed traveling through powered-off LCRY
broken in aabf7e060cb6

src/simulation/elements/PROT.cpp
@@ -64,9 +64,12 @@ int PROT_update(UPDATE_FUNC_ARGS)
 		break;
 	case PT_LCRY:
 		//Powered LCRY reaction: PROT->PHOT
-		part_change_type(i, x, y, PT_PHOT);
-		parts[i].life *= 2;
-		parts[i].ctype = 0x3FFFFFFF;
+		if (parts[under>>8].life > 5 && !(rand() % 10))
+		{
+			part_change_type(i, x, y, PT_PHOT);
+			parts[i].life *= 2;
+			parts[i].ctype = 0x3FFFFFFF;
+		}
 		break;
 	case PT_EXOT:
 		parts[under>>8].ctype = PT_PROT;