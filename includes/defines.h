7f9b39f984b88dc830a3a0b6d5d6fd575f89ebe1
jacob1 <jfu614@gmail.com>
2017-10-18 20:47:20 -0400

    really fix the mac version (untested)

--- includes/defines.h
+++ includes/defines.h
@@ -34,7 +34,7 @@
 #define MOD_VERSION 37
 #define MOD_MINOR_VERSION 2
 #define MOD_SAVE_VERSION 23 //This is not the version number of my mod anymore, it's only changed when I change the saving code
-#define MOD_BUILD_VERSION 88 //For update checks
+#define MOD_BUILD_VERSION 89 //For update checks
 
 #define MOBILE_MAJOR 1
 #define MOBILE_MINOR 3
