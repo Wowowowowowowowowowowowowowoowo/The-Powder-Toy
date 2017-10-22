9bfa96ed25922715062a15a07fa88d1b506a92c2
jacob1 <jfu614@gmail.com>
2017-10-22 12:51:31 -0400

    fix SPNG description error

--- src/simulation/elements/SPNG.cpp
+++ src/simulation/elements/SPNG.cpp
@@ -201,7 +201,7 @@ void SPNG_init_element(ELEMENT_INIT_FUNC_ARGS)
 	elem->DefaultProperties.temp = R_TEMP+0.0f +273.15f;
 	elem->HeatConduct = 251;
 	elem->Latent = 0;
-	elem->Description = "Sponge, absorbs water. Is not a moving solid.";
+	elem->Description = "Sponge, absorbs water. Is a moving solid.";
 
 	elem->Properties = TYPE_SOLID;
 
