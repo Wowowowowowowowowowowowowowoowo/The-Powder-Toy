b9a586c21e91a05ffbf6ba2d9bb07eec15496492
jacob1 <jfu614@gmail.com>
2019-08-10 17:21:23 -0400

    tpt++/cracker1000: Add .life serialization support to lsns b2adbb54db6a

--- src/simulation/elements/LSNS.cpp
+++ src/simulation/elements/LSNS.cpp
@@ -17,44 +17,108 @@
 
 int LSNS_update(UPDATE_FUNC_ARGS)
 {
-	int r, rx, ry, rt, rd = parts[i].tmp2;
+	int rd = parts[i].tmp2;
 	if (rd > 25) parts[i].tmp2 = rd = 25;
 	if (parts[i].life)
 	{
 		parts[i].life = 0;
-		for (rx=-2; rx<3; rx++)
-			for (ry=-2; ry<3; ry++)
+		for (int rx = -2; rx <= 2; rx++)
+			for (int ry = -2; ry <= 2; ry++)
 				if (BOUNDS_CHECK && (rx || ry))
 				{
-					r = pmap[y+ry][x+rx];
+					int r = pmap[y + ry][x + rx];
 					if (!r)
 						continue;
-					rt = TYP(r);
-					if (parts_avg(i,ID(r),PT_INSL) != PT_INSL)
+					int rt = TYP(r);
+					if (parts_avg(i, ID(r), PT_INSL) != PT_INSL)
 					{
-						if ((sim->elements[rt].Properties&PROP_CONDUCTS) && !(rt==PT_WATR||rt==PT_SLTW||rt==PT_NTCT||rt==PT_PTCT||rt==PT_INWR) && parts[ID(r)].life==0)
+						if ((sim->elements[rt].Properties&PROP_CONDUCTS) && !(rt == PT_WATR || rt == PT_SLTW || rt == PT_NTCT || rt == PT_PTCT || rt == PT_INWR) && parts[ID(r)].life == 0)
 						{
 							parts[ID(r)].life = 4;
 							parts[ID(r)].ctype = rt;
-							sim->part_change_type(ID(r),x+rx,y+ry,PT_SPRK);
+							sim->part_change_type(ID(r), x + rx, y + ry, PT_SPRK);
 						}
 					}
 				}
 	}
-	for (rx=-rd; rx<rd+1; rx++)
-		for (ry=-rd; ry<rd+1; ry++)
-			if (x+rx>=0 && y+ry>=0 && x+rx<XRES && y+ry<YRES && (rx || ry))
+	bool doSerialization = false;
+	bool doDeserialization = false;
+	int life = 0;
+	for (int rx = -rd; rx < rd + 1; rx++)
+		for (int ry = -rd; ry < rd + 1; ry++)
+			if (x + rx >= 0 && y + ry >= 0 && x + rx < XRES && y + ry < YRES && (rx || ry))
 			{
-				r = pmap[y+ry][x+rx];
-				if(!r)
-					r = photons[y+ry][x+rx];
-				if(!r)
+				int r = pmap[y + ry][x + rx];
+				if (!r)
+					r = photons[y + ry][x + rx];
+				if (!r)
 					continue;
-				if (parts[ID(r)].life > parts[i].temp-273.15)
-					parts[i].life = 1;
+
+				switch (parts[i].tmp)
+				{
+				case 1:
+					// .life serialization into FILT
+					if (TYP(r) != PT_LSNS && TYP(r) != PT_FILT && parts[ID(r)].life >= 0)
+					{
+						doSerialization = true;
+						life = parts[ID(r)].life;
+					}
+					break;
+				case 3:
+					// .life deserialization
+					if (TYP(r) == PT_FILT)
+					{
+						doDeserialization = true;
+						life = parts[ID(r)].ctype;
+					}
+					break;
+				case 2:
+					// Invert mode
+					if (TYP(r) != PT_METL && parts[ID(r)].life <= parts[i].temp - 273.15)
+						parts[i].life = 1;
+					break;
+				default:
+					// Normal mode
+					if (TYP(r) != PT_METL && parts[ID(r)].life > parts[i].temp - 273.15)
+						parts[i].life = 1;
+					break;
+				}
 			}
-	return 0;
-	
+
+	for (int rx = -1; rx <= 1; rx++)
+		for (int ry = -1; ry <= 1; ry++)
+			if (BOUNDS_CHECK && (rx || ry))
+			{
+				int r = pmap[y + ry][x + rx];
+				if (!r)
+					continue;
+				int nx = x + rx;
+				int ny = y + ry;
+				// .life serialization.
+				if (doSerialization)
+				{
+					while (TYP(r) == PT_FILT)
+					{
+						parts[ID(r)].ctype = 0x10000000 + life;
+						nx += rx;
+						ny += ry;
+						if (nx < 0 || ny < 0 || nx >= XRES || ny >= YRES)
+							break;
+						r = pmap[ny][nx];
+					}
+				}
+				// .life deserialization.
+				if (doDeserialization)
+				{
+					if (TYP(r) != PT_FILT)
+					{
+						parts[ID(r)].life = life - 0x10000000;
+						break;
+					}
+				}
+			}
+
+	return 0;	
 }
 
 void LSNS_init_element(ELEMENT_INIT_FUNC_ARGS)
