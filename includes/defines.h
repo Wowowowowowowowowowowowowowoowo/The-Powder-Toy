9982b1b7a85f27a9df033e8b1774b3c5ec779fc8
jacob1 <jfu614@gmail.com>
2019-08-17 11:40:16 -0400

    version 43.4

--- includes/defines.h
+++ includes/defines.h
@@ -36,9 +36,9 @@
 #define FAKE_MINOR_VER 1
 
 #define MOD_VERSION 43
-#define MOD_MINOR_VERSION 3
+#define MOD_MINOR_VERSION 4
 #define MOD_SAVE_VERSION 25 //This is not the version number of my mod anymore, it's only changed when I change the saving code
-#define MOD_BUILD_VERSION 114 //For update checks
+#define MOD_BUILD_VERSION 115 //For update checks
 
 #define MOBILE_MAJOR 1
 #define MOBILE_MINOR 7
