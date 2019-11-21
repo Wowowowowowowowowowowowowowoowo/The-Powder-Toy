1fe59e1ad727dc3473511dcec6cd3a7ee6bf6bff
jacob1 <jfu614@gmail.com>
2019-11-20 20:00:35 -0500

    ScrollWindows can now be scrolled on Android

--- src/interface/ScrollWindow.cpp
+++ src/interface/ScrollWindow.cpp
@@ -43,20 +43,41 @@ void ScrollWindow::OnDraw(gfx::VideoBuffer *buf)
 		if (scrolled > 0)
 			scrollPos = static_cast<float>(size.Y - scrollHeight) * (static_cast<float>(scrolled) / static_cast<float>(scrollSize));
 
-		buf->FillRect(size.X - 8, 0, 8, size.Y, 125, 125, 125, 100);
-		buf->FillRect(size.X - 8, static_cast<int>(scrollPos), 8, static_cast<int>(scrollHeight) + 1, 255, 255, 255, 255);
+		buf->FillRect(size.X - scrollbarWidth, 0, scrollbarWidth, size.Y, 125, 125, 125, 100);
+		buf->FillRect(size.X - scrollbarWidth, static_cast<int>(scrollPos), scrollbarWidth, static_cast<int>(scrollHeight) + 1, 255, 255, 255, 255);
 	}
 }
 
-void ScrollWindow::OnMouseDown(int x, int y, unsigned char button)
+void ScrollWindow::DoMouseDown(int x, int y, unsigned char button)
 {
 	if (isMouseInsideScrollbar)
 	{
 		scrollbarHeld = true;
-		initialClickPos = y;
+		initialClickX = x;
+		initialClickY = y;
 		initialOffset = scrolled;
 	}
+
+#ifndef TOUCHUI
+	Window::DoMouseDown(x, y, button);
+#else
+	if (!isMouseInsideScrollbar)
+		Window::DoMouseDown(x, y, button);
+#endif
 }
+
+#ifdef TOUCHUI
+void ScrollWindow::DoMouseUp(int x, int y, unsigned char button)
+{
+	if (!hasScrolled)
+	{
+		Window::DoMouseDown(initialClickX, initialClickY, button);
+		Window::DoMouseMove(x, y, x - initialClickX, y - initialClickY);
+	}
+	hasScrolled = false;
+	Window::DoMouseUp(x, y, button);
+}
+#endif
 
 void ScrollWindow::OnMouseUp(int x, int y, unsigned char button)
 {
@@ -67,20 +88,51 @@ void ScrollWindow::OnMouseMove(int x, int y, Point difference)
 {
 	if (scrollable)
 	{
+#ifndef TOUCHUI
 		float scrollHeight = static_cast<float>(size.Y) * (static_cast<float>(size.Y) / static_cast<float>(scrollSize + size.Y));
 		float scrollPos = 0;
 		if (scrolled > 0)
 			scrollPos = static_cast<float>(size.Y - scrollHeight) * (static_cast<float>(scrolled) / static_cast<float>(scrollSize));
 
-		isMouseInsideScrollbar = x >= size.X - 8 && x < size.X && y >= scrollPos && y < scrollPos + scrollHeight;
+		isMouseInsideScrollbar = x >= size.X - scrollbarWidth && x < size.X && y >= scrollPos && y < scrollPos + scrollHeight;
+#else
+		isMouseInsideScrollbar = true;
+#endif
 
-		if (scrollbarHeld)
+		if (scrollbarHeld && Subwindows.size() == 0)
 		{
 			int newScrolled;
-			if (x < 0)
+			if (x < initialClickX - 100)
 				newScrolled = initialOffset;
 			else
-				newScrolled = static_cast<float>(y - initialClickPos) / static_cast<float>(size.Y) * (scrollSize + size.Y) + initialOffset;
+			{
+#ifndef TOUCHUI
+				newScrolled = static_cast<float>(y - initialClickY) / static_cast<float>(size.Y) * (scrollSize + size.Y) + initialOffset;
+#else
+				// 10 pixels of leeway before it starts scrolling
+				int diff = initialClickY - y;
+				if (!hasScrolled)
+				{
+					if (std::abs(diff) < 10)
+						diff = 0;
+					else if (diff < 0)
+						diff += 10;
+					else if (diff > 0)
+						diff -= 10;
+					// Block events from being sent to components once we enter scrolling mode
+					if (diff != 0)
+					{
+						initialClickX = x;
+						initialClickY = y;
+						hasScrolled = true;
+						diff = 0;
+					}
+				}
+				newScrolled = static_cast<float>(diff) / static_cast<float>(size.Y) * (scrollSize + size.Y) + initialOffset;
+#endif
+			}
+			lastMouseX = x;
+			lastMouseY = y;
 			SetScrollPosition(newScrolled);
 		}
 	}
--- src/interface/ScrollWindow.h
+++ src/interface/ScrollWindow.h
@@ -12,19 +12,29 @@ class ScrollWindow : public Window
 	int lastMouseX, lastMouseY;
 
 	bool isMouseInsideScrollbar = false, scrollbarHeld = false;
-	int initialClickPos = 0, initialOffset = 0;
+	int initialClickX = 0, initialClickY = 0, initialOffset = 0;
+
+#ifdef TOUCHUI
+	bool hasScrolled = false;
+	const int scrollbarWidth = 0;
+#else
+	const int scrollbarWidth = 8;
+#endif
 
 public:
 	ScrollWindow(Point position, Point size);
 
-	int GetUsableWidth() { return size.X - 8; }
+	int GetUsableWidth() { return size.X - scrollbarWidth; }
 	void SetScrollable(bool scrollable, int maxScroll);
 	int GetScrollPosition() { return scrolled; }
 	void SetScrollPosition(int pos);
 	int GetMaxScrollSize() { return scrollSize; }
 
 protected:
-	void OnMouseDown(int x, int y, unsigned char button) override;
+	void DoMouseDown(int x, int y, unsigned char button) override;
+#ifdef TOUCHUI
+	void DoMouseUp(int x, int y, unsigned char button) override;
+#endif
 	void OnMouseUp(int x, int y, unsigned char button) override;
 	void DoMouseWheel(int x, int y, int d) override;
 	void OnDraw(gfx::VideoBuffer *buf) override;
