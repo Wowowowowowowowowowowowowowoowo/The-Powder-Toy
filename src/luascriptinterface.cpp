88535df73f90fa80d87dd00384f4ba93ff1cd3dc
jacob1 <jfu614@gmail.com>
2020-01-18 23:00:12 -0500

    HTTPRequest:status would never have returned queued since the request is started immediately on creation.

--- src/luascriptinterface.cpp
+++ src/luascriptinterface.cpp
@@ -3349,11 +3349,6 @@ public:
 		return dead || request->CheckDone();
 	}
 
-	bool Running() const
-	{
-		return !dead && request->CheckStarted();
-	}
-
 	void Progress(int *total, int *done)
 	{
 		if (!dead)
@@ -3404,13 +3399,9 @@ static int http_request_status(lua_State *l)
 	{
 		lua_pushliteral(l, "done");
 	}
-	else if (rh->Running())
-	{
-		lua_pushliteral(l, "running");
-	}
 	else
 	{
-		lua_pushliteral(l, "queued");
+		lua_pushliteral(l, "running");
 	}
 	return 1;
 }
