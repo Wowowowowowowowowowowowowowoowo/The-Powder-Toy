c62185a83c2556b25aed01c3163c57b7810decef
jacob1 <jfu614@gmail.com>
2020-01-30 00:27:04 -0500

    Build 118: actually fix linux libssl issue

--- includes/defines.h
+++ includes/defines.h
@@ -38,7 +38,7 @@
 #define MOD_VERSION 44
 #define MOD_MINOR_VERSION 0
 #define MOD_SAVE_VERSION 25 //This is not the version number of my mod anymore, it's only changed when I change the saving code
-#define MOD_BUILD_VERSION 117 //For update checks
+#define MOD_BUILD_VERSION 118 //For update checks
 
 #define MOBILE_MAJOR 1
 #define MOBILE_MINOR 7
