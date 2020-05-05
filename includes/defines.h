c6850394e53525165e0c33516defe239bc6856db
jacob1 <jfu614@gmail.com>
2020-05-04 21:07:30 -0400

    Haven't done a version in a while, so, this is now version 44.2

--- includes/defines.h
+++ includes/defines.h
@@ -36,13 +36,13 @@
 #define FAKE_MINOR_VER 0
 
 #define MOD_VERSION 44
-#define MOD_MINOR_VERSION 1
+#define MOD_MINOR_VERSION 2
 #define MOD_SAVE_VERSION 25 //This is not the version number of my mod anymore, it's only changed when I change the saving code
-#define MOD_BUILD_VERSION 119 //For update checks
+#define MOD_BUILD_VERSION 120 //For update checks
 
 #define MOBILE_MAJOR 1
-#define MOBILE_MINOR 8
-#define MOBILE_BUILD 114
+#define MOBILE_MINOR 9
+#define MOBILE_BUILD 115
 
 #define IDENT_VERSION "G" //Change this if you're not Simon! It should be a single letter
 
